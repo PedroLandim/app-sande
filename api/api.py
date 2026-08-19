3ce3a7419d7a27a0bd893251d3661b57c126fc8f
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2026-08-19 14:15:23 -0300

    Restringe CORS ao frontend

--- api/api.py
+++ api/api.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "https://dashboard-sande.onrender.com"
     ],
     allow_credentials=False,
     allow_methods=["*"],
